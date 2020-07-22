f3ff006910e69b6119dcafc47c9faf900ddf594a
Adam Małota <adammalota@mansio.pl>
2020-07-22 19:55:08 +0200

    Jupyter and data

--- main.py
+++ main.py
@@ -6,3 +6,13 @@ test = pd.read_csv('exoTest.csv')
 print('Dane treningowe\n', train.head())
 print('Dane testowe\n', test.head())
 
+x_train = train.drop('LABEL', axis=1)
+y_train = train.LABEL-1
+x_test = test.drop('LABEL', axis=1)
+y_test = test.LABEL-1
+
+# MODEL TFBT
+
+#numeric_column_headers = x_train.columns.values.tolist()
+
+#bc_fn = tf
---
+++ readme.md
@@ -0,0 +1,5 @@
+##Datasets
+https://www.kaggle.com/keplersmachines/kepler-labelled-time-series-data/data?select=exoTrain.csv
+
+
+
